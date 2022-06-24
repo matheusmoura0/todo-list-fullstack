1533c9bbe2b41fc9afab8ff09f0c8fabccdace36
Matheus <matheusmoura2110@gmail.com>
2022-06-24 13:28:52 -0300

    feat: add insert and read routes

--- server/app.js
+++ server/app.js
@@ -1,7 +1,31 @@
 const express = require('express');
-
 const app = express();
-const PORT = 3000;
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = 3002 || process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+//create
+app.post('/insert', (req, res) => {
+    
+});
+
+//read
+app.get('/read', (req, res) => {
+    console.log('entrou no read');
+});
+
+//update
+
+
+//delete
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
