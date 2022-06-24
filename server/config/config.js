75c873c13be03416d077007ae32de1599235e1a6
Matheus <matheusmoura2110@gmail.com>
2022-06-24 14:07:54 -0300

    feat: create todos migration

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ module.exports = {
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
+        dialect: "mysql",
         logging: true
     },
     test: {
