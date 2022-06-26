09b5b1d29cba7f34ba2e7eefc0ea1db3c4bce95b
Matheus <matheusmoura2110@gmail.com>
2022-06-26 14:46:45 -0300

    feat: create todo controller

--- server/controllers/TodosController.js
+++ server/controllers/TodosController.js
@@ -3,14 +3,19 @@ const { todos } = require('../models');
 export const findAll = async (req, res) => { 
     const Alltodos = await todos.findAll();
     res.json(Alltodos);
-}
+};
 
 export const getById = async (req, res) => {
     const todo = await todos.findByPk(req.params.id);
     res.json(todo);
-}
+};
 
 export const update = async (req, res) => { 
      await todos.update(req.body, { where: { id: req.params.id } });
     res.json({ message: 'Todo updated successfully' });
-}
+};
+
+export const create = async (req, res) => {
+    await todos.create(req.body);
+    res.json({ message: 'Todo created successfully' });
+};
